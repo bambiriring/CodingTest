1b02e164e0808b45c323db060279c968d78c4500
bambiriring <hmnicute1101@gmail.com>
2025-04-04 00:07:10 +0900

    Update 연탄 배달의 시작.py

--- Softeer/Level 1/연탄 배달의 시작.py
+++ Softeer/Level 1/연탄 배달의 시작.py
@@ -16,3 +16,18 @@ for i in range(0, n):
         elif loc[j]-loc[i]==min_dt:
             cnt +=1
 print(cnt)
+
+# https://softeer.ai/practice/7626
+
+import sys
+
+n = int(input())
+
+loc = list(map(int, input().split()))
+
+dis = []
+for i in range(1, n):
+    dis.append(loc[i] - loc[i-1])
+
+min_dis = min(dis)
+print(dis.count(min_dis))
