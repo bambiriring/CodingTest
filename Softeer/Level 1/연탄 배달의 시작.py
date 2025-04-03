8ee3683967f3d83510e672f815eb2f34dd5bcea9
bambiriring <hmnicute1101@gmail.com>
2025-04-04 00:10:52 +0900

    Update 연탄 배달의 시작.py

--- Softeer/Level 1/연탄 배달의 시작.py
+++ Softeer/Level 1/연탄 배달의 시작.py
@@ -31,3 +31,29 @@ for i in range(1, n):
 
 min_dis = min(dis)
 print(dis.count(min_dis))
+
+
+# https://softeer.ai/practice/7626
+
+import sys
+
+n = int(input())
+loc = list(map(int, input().split()))
+
+diff_count={}
+
+# 위치 정렬 (거리 계산을 위해 필요)
+loc.sort()
+
+for i in range(1, n):
+    diff = loc[i] - loc[i-1]
+
+    if diff in diff_count:
+        diff_count[diff] +=1
+    else:
+        diff_count[diff] = 1
+
+min_diff = min(diff_count.keys())
+
+# 최소 거리의 빈도 출력
+print(diff_count[min_diff])
